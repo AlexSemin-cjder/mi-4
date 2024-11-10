83abaf1ced61513246d0c2dc9554f6051a53c5e9
AlexSemin-cjder <119964665+AlexSemin-cjder@users.noreply.github.com>
2024-11-10 15:09:33 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,5 @@
+homework = 12
+hours_spent = 1.5
+Course_name = 'Python'
+time_for_one_task = hours_spent  / homework
+print('Курс:' + Course_name +', '  'всего задач:' + (str ( homework ) ) + ', ' + 'затрачено чясов:' + ( str( hours_spent)) + ', ' 'среднее время выполнения:' + ( str (time_for_one_task)) + ' часа' )
